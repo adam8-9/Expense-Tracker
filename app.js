a58e5127504d1d61cab4902b1240b783a0442044
adam8-9 <31065348+adam8-9@users.noreply.github.com>
2021-04-20 17:27:14 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,69 @@
+const place = document.querySelector(".location");
+const date = document.querySelector("#date")
+const amount = document.querySelector("#amount")
+const submit = document.querySelector("#submit")
+const clear = document.querySelector("#clear")
+const form = document.querySelector("form")
+const container = document.querySelector("#container")
+const dataContain = document.querySelector(".dataContain")
+
+
+
+/*form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    let newtoDo = toDo.value;
+    console.log(newtoDo);
+    let appendtodo = document.createElement("li")
+    appendtodo.append(newtoDo);
+    container.append(appendtodo);
+    form.elements.todo.value = "";
+})
+
+clear.addEventListener("click", function (e) {
+    e.preventDefault();
+    container.innerHTML = "";
+
+})*/
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    let places = place.value;
+    let dates = date.value;
+    let amounts = amount.value;
+    console.log(places);
+    console.log(dates);
+    console.log(amounts);
+
+    let data = document.createElement("tr")
+    let subdata1 = document.createElement("td")
+    let subdata2 = document.createElement("td")
+    let subdata3 = document.createElement("td")
+    let button = document.createElement("button")
+
+    button.innerHTML = "X";
+    subdata1.append(places)
+    subdata2.append(dates)
+    subdata3.append(`$${amounts}`, button)
+    data.append(subdata1, subdata2, subdata3)
+    dataContain.append(data)
+
+    //place.value = "";
+    // date.value = "";
+    //amount.value = "";
+    /*appendtodo.append(newtoDo);
+    container.append(appendtodo);
+    form.elements.todo.value = "";*/
+
+    button.addEventListener("click", function (r) {
+
+        button.parentElement.parentElement.remove();
+        console.log(r)
+    })
+
+})
+
+
+
+
+
+
